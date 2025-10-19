77d986f2bcf339bfe1b9b81773927a6e959e1753
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-10-19 19:32:19 +0200

    1. Operator functions and variables to update calc display later

---
+++ script.js
@@ -0,0 +1,26 @@
+// 1. Basic matematik operators functioner
+function plusFunction(num1, num2) {
+    return num1 + num2;
+}
+
+function minusFunction(num1, num2) {
+    return num1 - num2;
+}
+
+function gangeFunction(num1, num2) {
+    return num1 * num2;
+}
+    
+function dividereFunction(num1, num2) {
+    return num1 / num2;
+}
+
+// 2. Variables to update calculator display.
+let numberOne;
+let numberTwo;
+let operator; 
+
+// 3. A function that takes an operator, and two numbers and then calls one of the above functions on the numbers.
+function operate(operator, num1, num2) {
+
+}
