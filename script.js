0e49843ad437151dbde51e9b2b3892adab21deba
Jeppe Steen Nielsen <jsndesire@live.dk>
2025-10-21 20:08:25 +0200

    Html and addeventlistener on buttons

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function dividereFunction(num1, num2) {
 // 2. Variables to update calculator display.
 let numberOne;
 let numberTwo;
-let operator; 
+let operator;
 
-// 3. A function that takes an operator, and two numbers and then calls one of the above functions on the numbers - We make an object.
+// 3. We make an object of the operator functions.
 
 const operatorObject = {
     "+": plusFunction,
@@ -28,7 +28,20 @@ const operatorObject = {
     "*": gangeFunction,
     "/": dividereFunction
 }
+
+// A function that takes an operator, and two numbers and then calls one of the above functions on the numbers
 function operate(operator, num1, num2) {
     return operatorObject[operator](num1, num2);
 }
-console.log(operate("-", 10, 4));
+console.log(operate("*", 10, 4));
+
+// Function that populates display when buttons are clicked.
+const keyDigit = document.querySelector('.container');
+keyDigit.addEventListener("click", (e) => {
+   if (e.target.classList.contains("digit")) {
+    const value = e.target.dataset.value;
+    console.log(value);
+   }
+
+});
+
